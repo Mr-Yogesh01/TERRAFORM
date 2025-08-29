cf409858abc4b7aaae48f94fbb841a620d6f7287
Yogesh Vanjari <vanjariyogesh531@gmail.com>
2025-08-29 11:48:30 +0530

    DAY-14

---
+++ DAY-14-OpeanTofu/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  
+}
+
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name ="RAJ"
+  }
+}
+
+resource "aws_subnet" "Public-Sub" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name ="Public-sub"
+  }
+}
+
+resource "aws_subnet" "Private-Sub" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name ="private-sub"
+  }
+}

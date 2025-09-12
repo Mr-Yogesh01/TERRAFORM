20aa577d88727ba399fbcefc11b809bdb72ccdbc
Mr-Yogesh01 <yvanjari851@gmail.com>
2025-09-12 12:08:25 +0530

    Update main.tf

--- DAY-3/main.tf
+++ DAY-3/main.tf
@@ -1,14 +1,22 @@
-provider "aws" {
-  
+provider "aws" {  
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-0b09ffb6d8b58ca91"
-    instance_type = "t2.medium"
-    
-    tags = {
-      Name = "YOGI"
-    }
 
-  
+data "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
+
+data "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+}
+
+
+
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-0b09ffb6d8b58ca91"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  key_name               = "yogi.pem"
+}
+

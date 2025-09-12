ddeb42a399aff365bbd575bfe4a55cc3436e05dd
Mr-Yogesh01 <yvanjari851@gmail.com>
2025-09-12 12:23:23 +0530

    Update backend.tf

--- DAY-3/backend.tf
+++ DAY-3/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "yoghesh.xyz"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+ # backend "s3" {
+  #  bucket = "yoghesh.xyz"
+   # key    = "terraform.tfstate"
+    #region = "us-east-1"
+  #}
+#}
